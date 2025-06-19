1230920f489097026b94c7308437cb2cc565e6a4
Anik-Sarkar-01 <aniksarkar8910@gmail.com>
2025-06-19 23:57:45 +0600

    server created and connected to mongoose

---
+++ src/app.ts
@@ -0,0 +1,8 @@
+import express, { Application, Request, Response } from "express";
+const app: Application = express();
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Welcome to library management app!')
+})
+
+export default app;
