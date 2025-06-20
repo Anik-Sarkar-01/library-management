789681c34fb721f0d2b2a408526ac7ab43552751
Anik-Sarkar-01 <aniksarkar8910@gmail.com>
2025-06-21 01:05:23 +0600

    more validation added with custom error message in the bookschema and borrowschema

--- src/app/models/books.model.ts
+++ src/app/models/books.model.ts
@@ -4,25 +4,28 @@ import { IBooks } from "../interfaces/books.interface";
 const BookSchema = new Schema<IBooks>({
     title: {
         type: String,
-        required: true
+        required: [true, "Title is required."],
+        trim: true,
     },
     author: {
         type: String,
-        required: true
+        required: [true, "Author is required."],
+        trim: true
     },
     genre: {
         type: String,
         uppercase: true,
         enum: {
-            values : ['FICTION', 'NON_FICTION', 'SCIENCE', 'HISTORY', 'BIOGRAPHY', 'FANTASY'],
-            message: '{VALUE} is not supported'
+            values: ['FICTION', 'NON_FICTION', 'SCIENCE', 'HISTORY', 'BIOGRAPHY', 'FANTASY'],
+            message: '{VALUE} is not supported.'
         },
-        required: true
+        required: [true, 'Genre is required.']
     },
     isbn: {
         type: String,
-        required: true,
-        unique: true
+        required: [true, "ISBN is required."],
+        unique: true,
+        trim:true
     },
     description: {
         type: String,
@@ -31,7 +34,7 @@ const BookSchema = new Schema<IBooks>({
     copies: {
         type: Number,
         min: [0, 'Must be at least 0, got {VALUE}'],
-        required: true
+        required: [true, "COPIES is required."]
     },
     available: {
         type: Boolean,
--- src/app/models/borrow.model.ts
+++ src/app/models/borrow.model.ts
@@ -5,16 +5,16 @@ const borrowSchema = new Schema<IBorrow>({
     book: {
         type: Schema.Types.ObjectId,
         ref: 'Book',
-        required: true,
+        required: [true, "Book (borrowed book ID) is required."]
     },
     quantity: {
         type: Number,
-        required: true,
+        required: [true, 'Quantity is required.'],
         min: [1, 'Quantity must be at least 1, got {VALUE}']
     },
     dueDate: {
         type: Date,
-        required: true
+        required: [true, 'DueDate is required.']
     }
 },{
     versionKey: false,
